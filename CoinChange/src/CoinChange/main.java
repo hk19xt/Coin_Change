8ff202e807e241ce605c729358d470c907cf5501
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-08-21 17:11:14 -0400

    Update main.java

--- CoinChange/src/CoinChange/main.java
+++ CoinChange/src/CoinChange/main.java
@@ -1,1 +1,18 @@
+package CoinChange;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int sum = 4;
+        ArrayList<Integer> userCoins = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 2, 3));
+        ArrayList<Integer> partial = new ArrayList<>();
+
+        calculateCoins.calculate(sum, userCoins, partial);
+        calculateCoins.result();
+    }
+}
 
